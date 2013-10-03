38c59dddc5f7ecb7143f448831d6eba43566ba45
Oliver Heins <olli@sopos.org>
2013-10-03 21:55:29 +0200

    * This is a really big change.  Most has to do with the removal of   ambiguity, which is described in the latest change to   Parser/AbstractParser.php.  (7ad472e8cfb045a81b9180ecd4471d5a06aa8108)
    * new properties: private $shifts, private $processed.
    
    * __construct(): new optional parameter $rule.  In case of a closure,
      shift, or reduce the corresponding rule is added to the new state.
    
    * When shifting or reducing, a *new* state has to be build.
      Therefore, we need to *clone* it.  __clone() is implemented, which
      calls private function copyArray(array $array) (renamed from
      __myClone()).
    
    * new methods: public getProcessed(), public setProcessed(), private
      addShift(ParserState $state), public getShifts().  The latter
      returns the array of appended shifts and clears(!!!!!!) it.
    
    * closure(), shift(), reduce(): $rule is appended to the constructor
      of the new ParserState
    
    * new method: string public getHistory(), which returns a string
      representation of the parse tree of this element.
    
    * new method: array public getRank().  return value is described in
      7ad472e8cfb045a81b9180ecd4471d5a06aa8108.  First, precedence and
      associativity are stored in an array.  It then gets the parent
      elements of a symbol, where only the first and last are of any
      interest.  If there are none of these, an empty array is returned.
      Otherwise, it calls itself recursively on -- depending on the
      associativity -- the first (assoc == left) or the last (assoc ==
      right) element and appends the result to the array, which is
      returned.
    
    * new method: int protected countParserTokens(): counts all terminal
      symbols in the history of an ParserState.
    
    * new method: bool public equal(ParserRule $rule): returns if the
      state fulfills the given rule, starting from 0 (used to check if the
      state is in the grammar, and the only reasonable ParserRule to pass
      here is the start rule).

--- Parser/ParserState.php
+++ Parser/ParserState.php
@@ -10,32 +10,68 @@ class ParserState
     private $rule;
     private $reduction;
     private $value;
+    private $shifts;
+    private $processed;
 
-    public function __construct($x, array $ab, array $cd, $j)
+    public function __construct($x, array $ab, array $cd, $j, $rule = null)
     {
 	assert($x instanceof ParserToken);
 	assert(is_array($ab));
 	assert(is_array($cd));
 	assert(is_int($j));
+	$this->processed = false;
+	$this->shifts = array();
 	$x = clone $x;
-	$ab = $this->myClone($ab);
-	$cd = $this->myClone($cd);
+	$ab = $this->copyArray($ab);
+	$cd = $this->copyArray($cd);
 	$this->setX($x);
 	$this->setAb($ab);
 	$this->setCd($cd);
 	$this->setJ($j);
 	$this->setReduction($x->getReduction());
+	if ($rule !== null) {
+	    $this->setRule($rule);
+	}
     }
 
-    private function myClone(array $array)
+    public function getProcessed()
     {
-	$new = array();
+	return $this->processed;
+    }
 
-	foreach ($array as $key => $val) {
-	    $new[$key] = clone $val; 
+    public function setProcessed()
+    {
+	$this->processed = true;
+    }
+
+    private function addShift(ParserState $state)
+    {
+	$this->shifts[] = $state;
+    }
+
+    public function getShifts()
+    {
+	$shifts = $this->shifts;
+	$this->shifts = array();
+	return $shifts;
+    }
+
+    public function __clone()
+    {
+	$this->x = clone $this->x;
+	$this->ab = $this->copyArray($this->ab);
+	$this->cd = $this->copyArray($this->cd);
+    }
+
+    private function copyArray(array $array)
+    {
+	if ($array === null) {
+	    return null;
 	}
 
-	return $new;
+	$new = array();
+
+	return array_merge($new, $array);
     }
 
     public function setValue($value)
@@ -48,9 +84,8 @@ class ParserState
 	return $this->value or "";
     }
 
-    public function setX($x)
+    public function setX(ParserToken $x)
     {
-	assert($x instanceof ParserToken);
 	$this->x = $x;
     }
     
@@ -128,14 +163,13 @@ class ParserState
 	assert(is_int($i));
 	$cd = $this->getCd();
 	// x->ab.cd
-	$next_states = array_map(function($rule) use ($i) {
-		return new ParserState($rule->getSymbol(), [], $rule->getRule(), $i);
+	return array_map(function($rule) use ($i) {
+		return new ParserState($rule->getSymbol(), [], $rule->getRule(), $i, $rule);
 	    }, 
 	    array_values(array_filter($grammar, function($rule) use ($cd) { 
 			return count($cd) > 0 and $rule->getSymbol()->equal($cd[0]); 
 		    }
 		    )));
-	return $next_states;
     }
 
     public function shift (array $tokens, $i) 
@@ -147,15 +181,21 @@ class ParserState
 	$ab = $this->getAb();
 	$cd = $this->getCd();
 	$j = $this->getJ();
+	$rule = $this->getRule();
+	if (getType($tokens[$i]) == "string") {
+	    return null;
+	}
 	if (count($cd) > 0 and $cd[0]->compare($tokens[$i])) {
 	    $cd0 = new ParserToken(array("type"  => $tokens[$i]->getType(),
 					 "value" => $tokens[$i]->getValue()));
-	    return new ParserState($x, 
-				   array_merge($ab, array($cd0)), 
-				   array_slice($cd, 1), 
-				   $j);
+	    $this->addShift(new ParserState($x, 
+					    array_merge($ab, array($cd0)), 
+					    array_slice($cd, 1), 
+					    $j,
+					    $rule));
+	    return true;
 	} else {
-	    return Null;
+	    return false;
 	}
     }
 
@@ -168,35 +208,109 @@ class ParserState
 	$ab = $this->getAb();
 	$cd = $this->getCd();
 	$j = $this->getJ();
+	$clone = clone $this;
 	if (count($cd) != 0) { 
 	    // no possible reductions at this time
 	    return array();
 	}
 	$value = $this->computeValue();
 	$x->setValue($value);
-	return array_map(function($jstate) use ($x, $value) {
-		$newState = new ParserState($jstate->getX(), 
+	$x->addToHistory($clone);
+	return array_map(function($jstate) use ($x) {
+		return new ParserState($jstate->getX(), 
 					    array_merge($jstate->getAb(), array($x)), 
 					    array_slice($jstate->getCd(), 1), 
-					    $jstate->getJ());
-		return $newState;
+					    $jstate->getJ(),
+					    $jstate->getRule());
 	    },
 	    array_values(array_filter($chart[$j], function($jstate) use ($x) {
 			return count($jstate->getCd()) > 0 and $jstate->getCd()[0]->equal($x);
 		    }
 		    )));
-	return $newState;
+    }
+
+    public function getHistory()
+    {
+	$result = "";
+	$tokens = $this->getAb();
+	foreach ($tokens as $t) {
+	    $result .= " " . $t->getType();
+	    $history = $t->getHistory();
+	    if ($history instanceof ParserState) {
+		$result .= " (" . $history->getHistory() . ")";
+	    }
+	    $result .= " ";
+	}
+	return $result;
+    }
+
+    public function getRank()
+    {
+	$assoc = $this->getRule()->getAssociativity();
+	$prec = $this->getRule()->getPrecedence();
+	$val = array($prec, $assoc);
+	foreach ($this->getAb() as $sym) {
+	    $history = $sym->getHistory();
+	    if ($history instanceof ParserState) {
+		if (!isset($first)) {
+		    $first = $history;
+		} else {
+		    $last = $history;
+		}
+		$val[] = $history->countParserTokens();
+	    }
+	}
+	$result = array($val);
+	if (isset($first)) {
+	    //assert(isset($first) && isset($last));
+	    switch ($assoc) {
+	    case 'left':
+		$child = $first->getRank();
+		break;
+	    case 'right':
+		$child = $last->getRank();
+		break;
+	    default:
+		throw new Exception('Associativity Error: neither left nor right (' . $assoc .')');
+	    }
+	    foreach ($child as $val) {
+		$result[] = $val;
+	    }
+	} else {
+	    $result = array();
+	}
+
+	return $result;
+    }
+
+    protected function countParserTokens()
+    {
+	$result = 0;
+	foreach ($this->getAb() as $sym) {
+	    $history = $sym->getHistory();
+	    if ($history instanceof ParserState) {
+		$result += $history->countParserTokens();
+	    } else {
+		$result++;
+	    }
+	}
+	return $result;
     }
 
     private function computeValue()
     {
-	assert(count($this->getCd()) == 0);
+	//assert(count($this->getCd()) == 0);
 	$p = array(null); // p[0] : Value of x (to be computed)
 	foreach ($this->getAb() as $token) {
 	    $p[] = $token->getValue();
 	}
 	$p = $this->doReduction($p);
 	return $p[0];
+    }
+
+    public function equal(ParserRule $rule)
+    {
+	return $this->toString() == $rule->toString() . " .  from 0";
     }
 
     public function __toString()
