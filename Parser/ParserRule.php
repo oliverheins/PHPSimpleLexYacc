f07d128db4870ea05bb0729d157cab38d3feb3e1
Oliver Heins <olli@sopos.org>
2013-10-03 20:36:52 +0200

    * new methods: private $precedence, private $assoc.
    * precedence and associativity are optional arguments to the
      constructior: __construct($symbol, array $rule, $prec=0, $assoc=0).
      $assoc: 0 = left, 1 = right.
    
    * new methods: protected setPrecedence($prec), public getPrecedence(),
      protected setAssociativity($assoc = 0), public getAssociativity(),
      string public toString(), which calls ParserToken::getType() for every
      ParserToken and concatenates them to a string, which it returns

--- Parser/ParserRule.php
+++ Parser/ParserRule.php
@@ -7,12 +7,45 @@ class ParserRule
     private $symbol;
     private $rule;
     private $reduction;
+    private $precedence;
+    private $assoc;  // left = 0, right = 1
 
-    public function __construct($symbol, array $rule)
+    // $assoc: 0 = left, 1 = right
+    public function __construct($symbol, array $rule, $prec = 0, $assoc = 0)
     {
+	assert(is_int($prec));
+	assert(is_int($assoc));
 	$this->setSymbol($symbol);
+	$this->setPrecedence($prec);
+	$this->setAssociativity($assoc);
 	$this->setRule($rule);
 	$this->setReduction(is_callable($symbol->getReduction()) ? $symbol->getReduction() : null );
+    }
+
+    protected function setPrecedence($prec)
+    {
+	assert(is_int($prec));
+	$this->precedence = $prec;
+    }
+
+    public function getPrecedence()
+    {
+	$prec = $this->precedence;
+	assert(is_int($prec));
+	return $prec;
+    }
+
+    protected function setAssociativity($assoc = 0)
+    {
+	assert($assoc === 0 or $assoc === 1);
+	$this->assoc = $assoc;
+    }
+
+    public function getAssociativity()
+    {
+	$assoc = $this->assoc;
+	assert($assoc === 0 or $assoc === 1);
+	return $assoc === 0 ? 'left' : 'right';
     }
 
     public function setSymbol(ParserToken $symbol)
@@ -55,6 +88,20 @@ class ParserRule
 	    $rule .= $sym->__toString() . " ";
 	}
 	return $this->getSymbol()->__toString() . ": " . $rule;
+    }
+
+    public function toString()
+    {
+	$rule = '';
+	foreach ($this->getRule() as $sym) {
+	    if ($sym instanceof ParserToken) {
+		$rule .= $sym->getType() . " ";
+	    }
+	}
+
+
+	return $this->getSymbol()->getType() . ": "
+	    . $rule;
     }
 
 }
